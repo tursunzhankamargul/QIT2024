89acd0630b6dc186cf374b040466db5a86a7d02c
Kamargul <tursynzanovakamargul@gmail.com>
2024-10-30 12:54:43 +0500

    homework

---
+++ examples/30.10/aboutme.js
@@ -0,0 +1,11 @@
+let name = "Kamargul";
+let surname ="Tursynzhan";
+let age= 21;
+let birthday= 24.12;
+let school= "AmjiltCyberSchool";
+console.log("Atym");
+console.log("Tegim");
+console.log("Jasym");
+console.log("Tugan kunim");
+
+
